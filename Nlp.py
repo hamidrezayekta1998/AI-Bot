e5554314044e6a4998277c0e12ce26bcb2cc23ac
Hamidreza <Hamidrezayekta1998@gmail.com>
2020-11-29 14:38:36 +0330

    Just a small change in directory

--- Nlp.py
+++ Nlp.py
@@ -4,6 +4,7 @@ from hazm import *
 #Correcting the github account
 #Now my account works correctly it's compeltely ok
 
+
 stopwords=stopwords_list("Persian_stopwords.txt")
 sent='فردا هوای تهران و شیراز چند درجه  خواهد شد؟'
 print(sent)
