4527ecafb84d6a3810b415d1e1261e0b743975bf
hamidrezayekta1998 <75086863+hamidrezayekta1998@users.noreply.github.com>
2020-11-29 13:26:04 +0330

    Add files via upload

---
+++ Nlp.py
@@ -0,0 +1,16 @@
+from hazm import *
+import os
+os.chdir("F:/Project-AIbot")
+stopwords=stopwords_list("Persian_stopwords.txt")
+sent='فردا هوای تهران و شیراز چند درجه  خواهد شد؟'
+print(sent)
+normalizer = Normalizer()
+sent=normalizer.normalize(sent)
+token=word_tokenize(sent)
+sentence=[]
+for w in token:
+    if w not in stopwords:
+        sentence.append(w)
+print(sentence)
+for word in sentence:
+    print(Lemmatizer().lemmatize(word))
